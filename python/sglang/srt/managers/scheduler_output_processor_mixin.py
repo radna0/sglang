a80efdf878b09f57795334b07e40d5a9f188d5f6
David Wang <david.wang@modal.com>
2026-01-08 05:53:11 +0000

    native sglang backend
    (cherry picked from commit 99e140af07943fec8bb4f482bb7b8925411afc3f)

--- python/sglang/srt/managers/scheduler_output_processor_mixin.py
+++ python/sglang/srt/managers/scheduler_output_processor_mixin.py
@@ -82,6 +82,14 @@ class SchedulerOutputProcessorMixin:
                 details["storage_backend"] = self._get_storage_backend_type()
             return details
         return None
+    def _release_kv_cache_and_draft(
+        self: Scheduler, req: Req, *, is_insert: bool = True
+    ):
+        release_kv_cache(req, self.tree_cache, is_insert=is_insert)
+        draft_worker = getattr(self, "draft_worker", None)
+        hook = getattr(draft_worker, "on_req_finished", None) if draft_worker else None
+        if hook is not None:
+            hook(req)
 
     def process_batch_result_prebuilt(self: Scheduler, batch: ScheduleBatch):
         assert self.disaggregation_mode == DisaggregationMode.DECODE
@@ -96,7 +104,7 @@ class SchedulerOutputProcessorMixin:
                     req.rid,
                     thread_finish_flag=True,
                 )
-                release_kv_cache(req, self.tree_cache)
+                self._release_kv_cache_and_draft(req)
 
         # Note: Logprobs should be handled on the prefill engine.
         trace_slice_batch(RequestStage.DECODE_FAKE_OUTPUT, batch.reqs)
@@ -176,7 +184,7 @@ class SchedulerOutputProcessorMixin:
 
                     if req.finished():
                         self.maybe_collect_routed_experts(req)
-                        release_kv_cache(req, self.tree_cache)
+                        self._release_kv_cache_and_draft(req)
                         req.time_stats.completion_time = time.perf_counter()
                     elif not batch.decoding_reqs or req not in batch.decoding_reqs:
                         # This updates radix so others can match
@@ -310,7 +318,7 @@ class SchedulerOutputProcessorMixin:
                     req.check_finished()
 
                     if req.finished():
-                        release_kv_cache(req, self.tree_cache)
+                        self._release_kv_cache_and_draft(req)
                     else:
                         self.tree_cache.cache_unfinished_req(req)
                 else:
@@ -397,7 +405,7 @@ class SchedulerOutputProcessorMixin:
                 req.output_ids.append(next_token_id)
                 req.check_finished()
                 if req.finished():
-                    release_kv_cache(req, self.tree_cache)
+                    self._release_kv_cache_and_draft(req)
                     req.time_stats.completion_time = time.perf_counter()
                     break
 
@@ -475,9 +483,9 @@ class SchedulerOutputProcessorMixin:
                 if self.server_args.disaggregation_decode_enable_offload_kvcache:
                     # Asynchronously offload KV cache; release_kv_cache will be called after Device->Host transfer completes
                     if not self.decode_offload_manager.offload_kv_cache(req):
-                        release_kv_cache(req, self.tree_cache)
+                        self._release_kv_cache_and_draft(req)
                 else:
-                    release_kv_cache(req, self.tree_cache)
+                    self._release_kv_cache_and_draft(req)
 
                 req.time_stats.completion_time = time.perf_counter()
 
